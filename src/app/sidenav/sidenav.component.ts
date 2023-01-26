bc4c10af9f0e79193d18c88c4bf42e37ddfd0158
okomojacob <okomo.code@gmail.com>
2023-01-26 17:57:51 +0300

    Side nav conditionals and init

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,15 @@
+import { navBarData } from './nav-data';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  collapsed = false;
+  navbarData = navBarData
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
