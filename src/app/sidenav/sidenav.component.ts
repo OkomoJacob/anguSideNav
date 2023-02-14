0c0bf203659ef8ad28c3f856f94e72d1f44b2584
okomojacob <okomo.code@gmail.com>
2023-02-14 11:32:39 +0300

    Toggle view :pencil2:

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-  collapsed = false;
+  collapsed = true;
   navbarData = navBarData
   constructor() {}
 
