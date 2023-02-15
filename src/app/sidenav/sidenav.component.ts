062954139d95090e3f69f8ca5f3f3fae1844b8d7
okomojacob <okomo.code@gmail.com>
2023-02-15 17:57:02 +0300

    Updates :penci;2:

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,19 +1,25 @@
 import { navBarData } from './nav-data';
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
+interface SideNavToggle {
+  screenWidth: number,
+  collapsed: boolean,
+}
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
-  collapsed = true;
-  navbarData = navBarData
-  constructor() {}
 
-  @Output() 
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  navbarData = navBarData
+  screenWidth =  0;
+
   toggleCollapse(): void{
     this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
   }
 
   closeSideNav(): void{
