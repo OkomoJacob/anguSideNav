aace16a375baf8b2bda53b7bfe04aa1b21ced4af
okomojacob <okomo.code@gmail.com>
2023-02-16 09:15:12 +0300

    Updating app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CouponsComponent } from './coupons/coupons.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: products},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'coupens', component: CouponsComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'media', component: MediaComponent}
 ];
 
 @NgModule({
